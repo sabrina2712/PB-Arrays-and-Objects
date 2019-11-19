75134f58152baff5a0dc04bb81d1852cca337304
Oliver Weber <oliver@mondough.io>
2019-11-19 14:34:59 +0100

    Reverse

--- review.js
+++ review.js
@@ -53,3 +53,75 @@ const findVowels = (sentence) => {
 };
 
 console.log(findVowels("this is a string") === 4);
+
+/**
+ * **5. Missing Number.**
+Create a function that takes an array of all integers between 1 and 10 (excluding one) and returns the missing integer.
+
+Examples: 
+* missingNums([1, 2, 3, 4, 6, 7, 8, 9, 10]) ➞ 5
+* missingNums([7, 2, 3, 6, 5, 9, 1, 4, 8]) ➞ 10
+* missingNums([10, 5, 1, 2, 4, 6, 8, 3, 9]) ➞ 7 
+ */
+
+const sortNumber = (a, b) => {
+  return a - b;
+};
+
+// const missingNums = (arrayOfNums) => {
+//   let missingNumber = 0;
+//   arrayOfNums = arrayOfNums.sort(sortNumber);
+//   const expectation = [];
+
+//   for (let i = 1; i <= 10; i++) {
+//     expectation.push(i);
+//   }
+
+//   for (let index in expectation) {
+//     if (arrayOfNums[index] !== expectation[index]) {
+//       return expectation[index];
+//     }
+//   }
+
+//   return missingNumber;
+// };
+
+const missingNums = (arrayOfNums) => {
+  let missingNumber = 10;
+  arrayOfNums = arrayOfNums.sort(sortNumber);
+
+  for (let index in arrayOfNums) {
+    index = parseInt(index);
+
+    if (arrayOfNums[index] !== index + 1) {
+      missingNumber = index + 1;
+      break;
+    }
+  }
+
+  return missingNumber;
+};
+
+console.log(missingNums([1, 2, 3, 4, 6, 7, 8, 9, 10]), 5);
+console.log(missingNums([7, 2, 3, 6, 5, 9, 1, 4, 8]), 10);
+console.log(missingNums([10, 5, 1, 2, 4, 6, 8, 3, 9]), 7);
+
+/**
+ * **3. Reverse.**
+Create a function to reverse a number. 
+
+i.e. reverse(34532) ➞ 23543
+ */
+
+const reverse = (num) => {
+  const reverseNumberString = num
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+
+  return parseFloat(reverseNumberString) * Math.sign(num);
+};
+
+console.log(reverse(34532) === 23543);
+console.log(reverse(-34.532));
