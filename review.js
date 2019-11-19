42c96982755e565feaf17bc6985a6403051d4d5d
Oliver Weber <oliver@mondough.io>
2019-11-19 11:33:08 +0100

    Vowls count fixes #8

--- review.js
+++ review.js
@@ -24,3 +24,32 @@ const longestWord = (sentence = "") => {
 };
 
 console.log(longestWord("this is a web development course") === "development");
+
+/**
+ * **4. AEIOU: Vowels.**
+  Create a function that takes a string in its parameters and counts the number of vowels (i.e. in English, "a, e, i, o, u") in the string. 
+
+  i.e. findVowels("this is a string") ➞ 4
+ */
+
+const findVowels = (sentence) => {
+  let vowelsCount = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  for (let characterOfSentence of sentence) {
+    if (vowels.includes(characterOfSentence)) {
+      vowelsCount++;
+    }
+  }
+
+  // Basic for loop alternative
+  // for (let i = 0; i < sentence.length; i++) {
+  //   if (vowels.includes(sentence[i])) {
+  //     vowelsCount++;
+  //   }
+  // }
+
+  return vowelsCount;
+};
+
+console.log(findVowels("this is a string") === 4);
