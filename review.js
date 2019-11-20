f98ca7e5c378775b6043fffa20839812e6b50253
Oliver Weber <oliver@mondough.io>
2019-11-20 13:28:16 +0100

    amplified fixes #14

--- review.js
+++ review.js
@@ -294,7 +294,6 @@ Examples:
  */
 
 const monthName = (numberOfMonth) => {
-  debugger;
   numberOfMonth = numberOfMonth - 1;
   const monthOfYear = [
     "January",
@@ -317,3 +316,36 @@ const monthName = (numberOfMonth) => {
 console.log(monthName(3) === "March");
 console.log(monthName(12) === "December");
 console.log(monthName(6) === "June");
+
+/**
+ * **3. Amplify the Multiples of 4.**
+Create a function that takes an integer and returns an array of integers ascending from 1 to the given number, where:
+
+* For each number in the array that can be evenly divided by 4, that number is amplified by 10 (i.e. return 10 times the number).
+* If the number cannot be divided evenly by 4, simply return the number.
+* The given integer will always be equal to or greater than 1.
+* Include the given number (the number in the parameters).
+
+Examples:
+* amplify(4) ➞ [1, 2, 3, 40]
+* amplify(3) ➞ [1, 2, 3]
+* amplify(25) ➞ [1, 2, 3, 40, 5, 6, 7, 80, 9, 10, 11, 120, 13, 14, 15, 160, 17, 18, 19, 200, 21, 22, 23, 240, 25]
+ */
+
+const amplify = (number) => {
+  const amplified = [];
+
+  for (let i = 1; i <= number; i++) {
+    let element = i;
+    if (i % 4 === 0) {
+      element = element * 10;
+    }
+    amplified.push(element);
+  }
+
+  return amplified;
+};
+
+console.log(amplify(4));
+console.log(amplify(3));
+console.log(amplify(25));
